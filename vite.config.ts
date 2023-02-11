6cd8fa5be7c13764d0173f5d128f2d9f8687f8fd
neo <neo@daangn.com>
2023-02-11 23:55:33 +0900

    test23

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://fd4f62db329f7b545ffca53ea9d9b98f.r2.cloudflarestorage.com/cf-direct-upload-test',
   plugins: [react()],
 })
