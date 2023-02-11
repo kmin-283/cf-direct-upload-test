068f6e6ef44142ee3b33af394b47d70f4a6295ef
neo <neo@daangn.com>
2023-02-12 00:56:23 +0900

    test29

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,8 +1,13 @@
 export async function onRequest(context) {  
     const reqPath = context.functionPath;
+    console.log(context);
+    
     const obj = await context.env.BUCKET.get(reqPath);
+
+    console.log(obj);
+
     if (obj === null) {
       return new Response('Not found', { status: 404 });  
-    }  
+    }
     return new Response(obj.body);
   }
