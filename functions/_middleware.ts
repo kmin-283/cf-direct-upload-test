b89c1dd33f2604cfc55a0f1437fb2305c3cf8b2d
neo <neo@daangn.com>
2023-02-12 16:07:42 +0900

    test57

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,33 +1,36 @@
-import {PagesFunction, R2Bucket} from '@cloudflare/workers-types';
+import {EventContext, PagesFunction, R2Bucket} from '@cloudflare/workers-types';
 
 interface Env {
   CF_DIRECT_UPLOAD_TEST: R2Bucket;
 }
 
 
-export const onRequestGet: PagesFunction<Env | any> = async (context) => {  
-  const url = new URL(context.request.url);
-  const obj = await context.env.CF_DIRECT_UPLOAD_TEST.get(`cf-direct-upload-test${url.pathname}`);
+export const onRequestGet = async (context) => {  
+  // const url = new URL(context.request.url);
+  // const obj = await context.env.CF_DIRECT_UPLOAD_TEST.get(`cf-direct-upload-test${url.pathname}`);
 
-  const v = context.request.headers.keys()
+  // const v = context.request.headers.keys()
   
-  while (!v.next().done) {
-    console.log(v.next().value);
-  }
+  // while (!v.next().done) {
+  //   console.log(v.next().value);
+  // }
   
-  if (obj) {
-    console.log('obj', obj.body);
-    return new Response(obj.body as any, {
-      headers: {
-        'Content-Type': 'application/javascript',
-      }
-    });
-  }
+  // if (obj) {
+  //   console.log('obj', obj.body);
+  //   return new Response(obj.body as any, {
+  //     headers: {
+  //       'Content-Type': 'application/javascript',
+  //     }
+  //   });
+  // }
 
   try {
     const res = await context.env.ASSETS.fetch(context.request.url);
 
-    console.log('res', res);
+    if (context.request.url.slice(-3) === '.js') {
+      throw new Error('find assets in r2 storage');
+    }
+
     if (res.ok) {
       return res;
     } else {
@@ -36,12 +39,16 @@ export const onRequestGet: PagesFunction<Env | any> = async (context) => {
   } catch (error) {
     const url = new URL(context.request.url);
     const obj = await context.env.CF_DIRECT_UPLOAD_TEST.get(url.pathname);
+    
     console.log('obj', obj);
-    if (!obj) {
+
+    if (obj === null) {
       return new Response('Not found', { status: 404 });  
     }
-    return new Response(obj.body as any);
-  }
 
-  return context.next();
+    return new Response(obj.body as any, {
+      headers: {
+        'Content-Type': 'application/javascript',
+    }});
+  }
 }
