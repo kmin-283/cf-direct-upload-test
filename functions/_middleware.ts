4ab79a865a72d580a50c4a60765afc5c9bd2b2ac
neo <neo@daangn.com>
2023-02-12 14:16:04 +0900

    test43

---
+++ functions/_middleware.ts
@@ -0,0 +1,27 @@
+import {PagesFunction, R2Bucket} from '@cloudflare/workers-types';
+
+interface Env {
+  CF_DIRECT_UPLOAD_TEST: R2Bucket;
+}
+
+
+export const onRequestGet: PagesFunction<Env | any> = async (context) => {  
+  try {
+    const res = await context.env.ASSETS.fetch(context.request.url);
+
+    if (res.ok) {
+      return res;
+    }
+  } catch (error) {
+    const obj = await context.env.CF_DIRECT_UPLOAD_TEST.get(context.request.url);
+
+    if (!obj) {
+      return new Response('Not found', { status: 404 });  
+    }
+
+    console.log(obj);
+    return new Response(obj.body as any);
+  }
+
+  return context.next();
+}
