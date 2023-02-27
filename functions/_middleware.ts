c52859e01cab7f647b073773bb82e2928bd99383
neo <neo@daangn.com>
2023-02-27 20:13:02 +0900

    test...4

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,45 +1,80 @@
-import {EventContext, PagesFunction, R2Bucket} from '@cloudflare/workers-types';
+import { R2Bucket, Cache, PagesFunction } from '@cloudflare/workers-types'
+import { lookup } from 'mrmime'
 
 interface Env {
-  CF_DIRECT_UPLOAD_TEST: R2Bucket;
+  CF_DIRECT_UPLOAD_TEST: R2Bucket
 }
 
+export const onRequestGet: PagesFunction<Env> = async (context) => {
+  const url = new URL(context.request.url)
 
-export const onRequestGet = async (context) => {
-  const url = new URL(context.request.url);
+  const cache: Cache = (caches as any)?.default
 
+  const responseHeaders = new Headers()
 
-  // cloudflare pages 도메인에 대한 asset get 요청만 처리해야 함.
-  console.log('url', url);
-  console.log('url', url.host);
-  if (url.host !== 'cf-direct-upload-test.pages.dev') {
-    return context.next();
-  }
-
-  try {
-    const res = await context.env.ASSETS.fetch(context.request.url);
-
-    // cloudflare에서 asset을 가져올 때 해당 에셋을 찾지 못하면 자동으로 기본 index.html을 가져오는 문제가 있음.  
-    // 그래서 청크에러가 발생해야하는 상황에서도 200으로 응답이 떨어지는 문제가 있음.
-
-    if (url.pathname.includes('assets') && res.headers.get('content-type').includes("text/html")) {
-      if (context.request.url.slice(-4) !== 'html') {
-        throw new Error('find assets in r2 storage');
+  if (url.pathname.includes('assets')) {
+    if (cache) {
+      const response = await cache.match(context.request)
+      if (response) {
+        return response
       }
     }
 
-    return res
-  } catch (error) {
-    const obj = await context.env.CF_DIRECT_UPLOAD_TEST.get(`cf-direct-upload-test${url.pathname}`);
-  
-    if (obj === null) {
-      return new Response('Not found', { status: 404 });  
+    const contentType = lookup(url.pathname.slice(1))
+
+    responseHeaders.set('Content-Type', contentType || 'application/octet-stream')
+
+    switch (contentType) {
+      case 'text/html':
+      case 'application/json': {
+        responseHeaders.set('Cache-Control', 'public, max-age=0, must-revalidate')
+        break
+      }
+      default: {
+        responseHeaders.set('Cache-Control', 'public, max-age=86400')
+        break
+      }
+    }
+  } else if (url.pathname === '/' || url.pathname === '/index.html') {
+    responseHeaders.set('Content-Type', 'text/html')
+  }
+
+  try {
+    const res = await context.env.ASSETS.fetch(context.request.url)
+
+    // cloudflare에서 asset을 가져올 때 해당 에셋을 찾지 못하면 자동으로 기본 index.html을 가져오는 문제가 있음.
+    // 그래서 청크에러가 발생해야하는 상황에서도 200으로 응답이 떨어지는 문제가 있음.
+
+    if (url.pathname.includes('assets') && res.headers.get('content-type')?.includes('text/html')) {
+      if (responseHeaders.get('content-type') !== 'text/html') {
+        throw new Error('find assets in r2 storage')
+      }
     }
 
-    // file extension에 따라 content-type을 다르게 설정해줘야함. 종류는 js와 이미지 에셋이 있음.
-    return new Response(obj.body as any, {
-      headers: {
-        'Content-Type': 'application/javascript',
-    }});
+    const newRes = new Response(res.body, {
+      ...res,
+      headers: responseHeaders,
+    })
+
+    // html이 아니면 캐싱
+    if (responseHeaders.get('content-type') !== 'text/html') {
+      context.waitUntil(cache.put(context.request, newRes.clone()))
+    }
+    return newRes
+  } catch (error) {
+    const obj = await context.env.CF_DIRECT_UPLOAD_TEST.get(`cf-direct-upload-test${url.pathname}`)
+
+    console.log('get resource from r2')
+    if (obj === null) {
+      return new Response('Not found', { status: 404 })
+    }
+
+    const newRes = new Response(obj.body, {
+      status: 200,
+      headers: responseHeaders,
+    })
+
+    context.waitUntil(cache.put(context.request, newRes.clone()))
+    return newRes
   }
 }
